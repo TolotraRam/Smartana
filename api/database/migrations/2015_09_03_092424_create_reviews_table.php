171684808885440c6a595fa882dc217c7c7159c2
Tolotra Ramahatoraka <tolotra.ramahatoraka@bocasay.fr>
2015-10-16 09:55:29 +0300

    Migration table reviews

--- api/database/migrations/2015_09_03_092424_create_reviews_table.php
+++ api/database/migrations/2015_09_03_092424_create_reviews_table.php
@@ -15,19 +15,20 @@ class CreateReviewsTable extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
-            $table->integer('store_id');
+            $table->string('store_id');
             $table->integer('score');
             $table->text('message');
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned();
             $table->timestamp('published_at');
         });
+
         Schema::table('reviews', function(Blueprint $table) {
-            /*$table->foreign('store_id')->references('store_id')->on('venues')
+            $table->foreign('store_id')->references('store_id')->on('venues')
                 ->onDelete('restrict')
-                ->onUpdate('restrict');*/
-            /*$table->foreign('user_id')->references('id')->on('user')
+                ->onUpdate('restrict');
+            $table->foreign('user_id')->references('id')->on('user')
                 ->onDelete('restrict')
-                ->onUpdate('restrict');*/
+                ->onUpdate('restrict');
         });
     }
 
